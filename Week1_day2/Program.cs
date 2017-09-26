720f3a8d910f90f599de7d51f735da9bbfd45ad5
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-09-26 15:15:05 -0400

    Definitions of Operators

--- Week1_day2/Program.cs
+++ Week1_day2/Program.cs
@@ -32,6 +32,46 @@ namespace Week1_day2
             Console.WriteLine(partialNum);
             Console.WriteLine(someNumber);
 
+            // ARITHMETIC operators
+            // Operators + adds two opperands
+            // - subtracts second opperand from the first
+            // * multplies both opperands
+            // / (as an operator) divides the first opperand by the second opperand
+            // $ - Modulus - checks the remainder (and divides)
+
+            // RELATIONAL Operators
+            // ++ Increments opperand by 1 and is used for int (integer)
+            // -- Decrements opperand by 1 
+            // == Checks if the values of two opperands are equal
+            // if they are equal, returns a "bool true"
+            // ! (bang) =  checks to see if the value of two opperands is NOT equal
+            // if they are NOT equal, returns a "bool true"
+            // > checks to see if the value of the left opperant is greater than the right
+            // if LEFT is greater, then condition is true
+            // < checks to see if the value of the LEFT is  less than the value of the RIGHT opperand
+            // if LEFT is LESS than the value of the RIGHT then the condition becomes true
+            // < = (less than or equal to) 
+                // checks to see if the value of the LEFT opperand is less than or equal to the value of the right
+                // if so, then condition is true
+            // > = (greater than or equal to)
+            // checks to see if the value of the LEFT opperand is greater than or equal to the value on the right
+            // if so, then true
+
+            // LOGICAL Operators
+            /*
+             * && we call AND operator. If both the opperands are
+             * non zero then the condition becomes true
+             * 
+             * || we call OR operator. If any of the two opperands is none zero 
+             * then the condition becomes true
+             * 
+             * ! we call NOT operator. This is used to reverse the logiacal state of the opperand. If a condition 
+
+
+
+
+
+
 
 
 
